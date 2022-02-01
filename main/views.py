caef687273ba711f193733c9be5d044bbacee977
Nayan <ndas5144@gmail.com>
2022-02-01 22:58:03 +0530

    deleted all files

--- main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import redirect, render, HttpResponse
-
-
-# Create your views here.
-
-def index(request):
-    return render(request, 'home.html')
-    # return HttpResponse('<h1>index</h1>')
