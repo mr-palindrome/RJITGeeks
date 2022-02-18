854b099c7839d5d0ec2b30f759064e2541a57339
Nayan Das <56421861+mr-palindrome@users.noreply.github.com>
2022-02-18 15:10:42 +0530

    Update settings.py

--- RJITGeeks/settings.py
+++ RJITGeeks/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1','localhost','rjitgeeks.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rjitgeeks.herokuapp.com']
 
 
 # Application definition
