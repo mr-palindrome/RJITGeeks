f34e5246f7dec02aff4e371539b2644be566b651
Nayan <ndas5144@gmail.com>
2022-02-18 12:43:08 +0530

    canged environmental variables

--- RJITGeeks/settings.py
+++ RJITGeeks/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-_33703#*@%8arj#h$(u3eb&@q%mz$542oyrsb%lxctavd43@sm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','loclhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','rjitgeeks.herokuapp.com']
 
 
 # Application definition
@@ -80,10 +81,10 @@ WSGI_APPLICATION = 'RJITGeeks.wsgi.application'
 DATABASES = {
         'default': {
             'ENGINE': 'djongo',
-            'NAME': config('DATABASE'),
+            'NAME': 'RJITGeeksData', #config('DATABASE'),
             'ENFORCE_SCHEMA': False,
             'CLIENT': {
-                'host': config('HOST')
+                'host': 'mongodb+srv://RJITGeeks:RJITGeeks123geeks@rjitgeeks.4jxdh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', #config('HOST')
             }
         }
 }
